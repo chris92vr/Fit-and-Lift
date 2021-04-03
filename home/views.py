01904b26ded36c4ad50dfb74bfdd727e1f4de660
chris92vr <garofolichristian@gmail.com>
2021-04-03 10:57:57 +0000

    Improved GUI, added contact_us_success template

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .forms import ContactForm
 from django.conf import settings
 from django.core.mail import send_mail
+from profiles.models import UserProfile
 
 
 def index(request):
@@ -10,18 +11,20 @@ def index(request):
 
 
 def contact(request):
+    """ Renders a contact form """
     form = ContactForm(request.POST or None)
+    profile = get_object_or_404(UserProfile, user=request.user)
     if form.is_valid():
-        subject= form.cleaned_data.get("subject")
+        subject = form.cleaned_data.get("subject")
         contact_email = form.cleaned_data.get("contact_email")
         content = form.cleaned_data.get("content")
-        content = " with the email, " + contact_email + ", sent the following message:\n\n" + content;
-        send_mail(subject, content, settings.DEFAULT_FROM_EMAIL, [settings.DEFAULT_FROM_EMAIL])
-        context = {'form': form}
-        return render(request, 'home/contact_us.html', context)
+        content = " with the email, " + contact_email + \
+            ", sent the following message:\n\n" + content
+        send_mail(
+            subject, content, settings.DEFAULT_FROM_EMAIL, [
+                settings.DEFAULT_FROM_EMAIL])
+        context = {'profile': profile}
+        return render(request, 'home/contact_us_success.html', context)
     else:
         context = {'form': form}
         return render(request, 'home/contact_us.html', context)
-
-
-
