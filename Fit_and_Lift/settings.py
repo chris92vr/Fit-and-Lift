865072d83fe7953dae61b7a6bcb105f98e996645
chris92vr <garofolichristian@gmail.com>
2021-04-04 07:45:12 +0000

    Added STATIC_ROOT path

--- Fit_and_Lift/settings.py
+++ Fit_and_Lift/settings.py
@@ -74,7 +74,8 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # required by allauth
+                # required by allauth
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
@@ -168,9 +169,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- Fit_and_Lift/urls.py
+++ Fit_and_Lift/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +12,8 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('blog/', include('blog.urls')),
+    path('static/<path:path>/', serve, {'document_root': settings.STATIC_ROOT, }),
+    path('media/<path:path>/', serve, {'document_root': settings.MEDIA_ROOT, }),
+
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
