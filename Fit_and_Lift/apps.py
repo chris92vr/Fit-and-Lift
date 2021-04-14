f0a908f34cc7dae9b97c93443b3c121ce74af5e5
chris92vr <garofolichristian@gmail.com>
2021-04-14 23:17:00 +0000

    Set DEBUG=development

--- Fit_and_Lift/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class SubscriptionConfig(AppConfig):
-    name = 'Fit_and_Lift'
-
-    def ready(self):
-        from . import updater
-        updater.start()
--- Fit_and_Lift/updater.py
+++
@@ -1,8 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from .deleted_expired_subscriptions import cleanup_expired_subscriptions
-
-
-def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(cleanup_expired_subscriptions, 'interval', seconds=10)
-    scheduler.start()
