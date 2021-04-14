9ec71ecf57cb4c93e04eba6d0406849579699742
chris92vr <garofolichristian@gmail.com>
2021-04-14 13:01:45 +0000

    Correcte django crontab function for delete expired subscriptions

--- Fit_and_Lift/cron.py
+++ Fit_and_Lift/cron.py
@@ -5,6 +5,6 @@ from datetime import datetime, timedelta
 def cleanup_expired_subscriptions():
     # Deleted expired subscriptions
     Subscription.objects.filter(
-            expire_date_subscription__lte=datetime.now() +
+            expire_date_subscription__lte=datetime.now() -
             timedelta(
                 days=1)).delete()
