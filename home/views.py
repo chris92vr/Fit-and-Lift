eb5791cd03482e9e3a3732c0f119f0512375a015
chris92vr <garofolichristian@gmail.com>
2021-04-14 23:16:19 +0000

    Added controll expired subscription index view

--- home/views.py
+++ home/views.py
@@ -5,10 +5,23 @@ from django.core.mail import send_mail
 from profiles.models import UserProfile
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from membership.models import UserMembership, Subscription
+import datetime as dt
+from profiles.models import UserProfile
 
 
 def index(request):
     """ A view to return the index page """
+    date = dt.date.today()
+    profile = UserProfile.objects.get(user=request.user)
+    subscriptions_expired=Subscription.objects.filter(
+            expire_date_subscription__lt=date)
+    for subscription in subscriptions_expired:
+        usermembership = get_object_or_404(
+        UserMembership, member_profile=profile)
+        usermembership.delete()
+    subscriptions_expired.delete()
+    
     return render(request, 'home/index.html')
 
 
